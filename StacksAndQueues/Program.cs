549d180e7fd5917a50032639250ac99863ff549f
im4kshay <akshaysayre7@gmail.com>
2022-04-12 02:01:04 +0530

    Ability to create a Stack of 56->30->70

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -12,6 +12,16 @@ namespace StacksAndQueues
         {
             //Welcome Message
             Console.WriteLine("=-=-=-=-=-Welcome To The Stacks And Queues Program-=-=-=-=-=");
+            Console.WriteLine("----------------------------");
+
+            //Calling Class and Adding Data in Stack
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+
+            //Display Message
+            linkedListStack.Display();
 
             Console.ReadLine();
         }
