8974ae233df54dd2f1af38c01310574bc9147dad
im4kshay <akshaysayre7@gmail.com>
2022-04-12 01:48:33 +0530

    Add project files.

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=-=-=-=-=-Welcome To The Stacks And Queues Program-=-=-=-=-=");
+        }
+    }
+}
