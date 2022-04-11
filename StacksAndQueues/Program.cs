ecad70954f519afa467b94adc47a5dc2af637162
im4kshay <akshaysayre7@gmail.com>
2022-04-12 02:06:14 +0530

    Ability to peak and pop from the Stack till it is empty 56->30->70

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -19,9 +19,15 @@ namespace StacksAndQueues
             linkedListStack.Push(70);
             linkedListStack.Push(30);
             linkedListStack.Push(56);
+            Console.WriteLine("----------------------------");
 
-            //Display Message
-            linkedListStack.Display();
+            //Peek Value from Stack
+            linkedListStack.Peek();
+            Console.WriteLine("----------------------------");
+            linkedListStack.Pop();
+
+            //Pop value till Stack will empty
+            linkedListStack.IsEmpty();
 
             Console.ReadLine();
         }
